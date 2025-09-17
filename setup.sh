dd2491352c3c77dc796d1d3b7dce0580a80cc005
Charles Ueltschey <chaseuelt@gmail.com>
2025-09-17 08:44:51 -0500

    Working vim install script

---
+++ setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ $EUID -eq 0]; then
+	echo "This script should not be run as root!"
+	exit 0
+fi
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+cat << EOF > ~/.vimrc
+call plug#begin('~/.vim/plugged')
+
+Plug 'junegunn/fzf.vim'
+Plug 'jremmen/vim-ripgrep'
+
+call plug#end()
+
+nnoremap <Space><Space> :Files<CR>
+nnoremap <Space>sg :Rg<Space>
+
+colorscheme habamax
+EOF
+
+echo "Install Complete"
